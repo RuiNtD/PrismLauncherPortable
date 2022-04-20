e4ee16cef10857610ab692be1f5f04eda96c28d2
Fayne Aldan <FayneAldan@gmail.com>
2022-04-20 10:58:20 -0600

    Added batch tools

---
+++ make_installer.bat
@@ -0,0 +1,4 @@
+@echo off
+set dir=%CD%
+cd \PortableApps\PortableApps.comInstaller
+start PortableApps.comInstaller.exe %dir%\PolyMCPortable
---
+++ make_launcher.bat
@@ -0,0 +1,4 @@
+@echo off
+set dir=%CD%
+cd \PortableApps\PortableApps.comLauncher
+start PortableApps.comLauncherGenerator.exe %dir%\PolyMCPortable
